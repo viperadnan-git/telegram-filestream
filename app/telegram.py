bdc5aa3911006afc00485c0d1e3aef2fc0faf78d
viperadnan-git <viperadnan@gmail.com>
2024-06-01 23:02:11 +0000

    add sender mentions in log

--- app/telegram.py
+++ app/telegram.py
@@ -4,6 +4,7 @@ import traceback
 
 import humanize
 from telethon import events
+from telethon.errors import MediaCaptionTooLongError, MessageTooLongError
 
 from app import cache, chat_id, client, expiry, public_url
 from app.encrypter import string_encryptor
@@ -13,20 +14,30 @@ from app.utils import get_file_name, get_message_info
 async def handle_message(evt: events.NewMessage.Event) -> None:
     if not evt.is_private:
         return
+    
     if not evt.file:
         await evt.respond(
             "<b>Send any document 📁🗂️🎥🎤🖼️ to get direct download link.</b>"
         )
         return
-    msg = await client.send_message(chat_id, evt.message)
-    file_name = get_file_name(msg)
-    message_hash = string_encryptor.encrypt(str(msg.id))
+    
+    sender_name = evt.message.sender.first_name + " " + evt.message.sender.last_name if evt.message.sender.last_name else evt.message.sender.first_name
+    sender_mention = f"<a href='tg://user?id={evt.message.sender_id}'>👤 {sender_name}</a>"
+    saved_message_caption = f"{evt.message.text}\n\n{sender_mention}"
+    
+    try:
+        saved_message = await client.send_file(chat_id, evt.message.media, caption=saved_message_caption, buttons=evt.message.reply_markup)
+    except (MediaCaptionTooLongError, MessageTooLongError):
+        saved_message = await client.send_file(chat_id, evt.message.media, caption=sender_mention, buttons=evt.message.reply_markup)
+    
+    file_name = get_file_name(saved_message)
+    message_hash = string_encryptor.encrypt(str(saved_message.id))
     url = f"{public_url}/{message_hash}/{file_name}"
     await evt.respond(
         f"<b>🔗 Link to download file\n\n</b> <a href='{url}'>{file_name}</a>\n\n<i>This link will expire in {humanize.naturaldelta(expiry)}</i>"
     )
 
-    cache[message_hash] = get_message_info(msg)
+    cache[message_hash] = get_message_info(saved_message)
 
 
 async def register_handler():
